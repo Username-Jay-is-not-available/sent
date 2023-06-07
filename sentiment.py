0e8e7a8ed39e4ec1d04a0a342f40fe5fa6d85ac6
seanyangx <98291896+seanyangx@users.noreply.github.com>
2023-06-06 21:30:23 -0700

    completed process_text and build_vocab

--- sentiment.py
+++ sentiment.py
@@ -1,13 +1,17 @@
 # CS331 Sentiment Analysis Assignment 3
 # This file contains the processing functions
-
+import string
+import re
 
 def process_text(text):
     """
     Preprocesses the text: Remove apostrophes, punctuation marks, etc.
     Returns a list of text
     """
-
+    text = text.lower()  # Convert to lower case
+    text = re.sub(r'\d+', '', text)  # Remove digits
+    text = text.translate(str.maketrans('', '', string.punctuation))  # Remove punctuation
+    preprocessed_text = text.split()  # Split into words
     return preprocessed_text
 
 
@@ -17,7 +21,8 @@ def build_vocab(preprocessed_text):
     preprocessed_text: output from process_text
     Returns unique text tokens
     """
-
+    #removed duplicated words from preprocessed_text and sorted it
+    vocab = sorted(set(preprocessed_text))
     return vocab
 
 
@@ -28,7 +33,6 @@ def vectorize_text(text, vocab):
     vocab: vocab from build_vocab
     Returns the vectorized text and the labels
     """
-
     return vectorized_text, labels
 
 
