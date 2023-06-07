1343e1deb16265e41647a6c10cf0f3f40757fec5
seanyangx <98291896+seanyangx@users.noreply.github.com>
2023-06-06 21:19:38 -0700

    added comments

--- classifier.py
+++ classifier.py
@@ -23,36 +23,45 @@ class BayesClassifier():
         train_labels: vectorized labels
         vocab: vocab from build_vocab
         """
+        #init variables for positive and negative words AND negative and negative sentences
         total_positive_words = 0
         total_negative_words = 0
         total_positive_sentences = 0
         total_negative_sentences = 0
 
+        #loop through training data
         for sentence, label in zip(train_data, train_labels):
+            #if positive (1)
             if label == 1:
                 total_positive_sentences += 1
+                #add positive words into list
                 for word in sentence:
                     if word not in self.postive_word_counts:
                         self.postive_word_counts[word] = 0
                     self.postive_word_counts[word] += 1
                     total_positive_words += 1
+            #if negative (0)
             else:
                 total_negative_sentences += 1
+                #add negative words into list
                 for word in sentence:
                     if word not in self.negative_word_counts:
                         self.negative_word_counts[word] = 0
                     self.negative_word_counts[word] += 1
                     total_negative_words += 1
 
+        #calculate ratio of positive and negative sentences
         self.percent_positive_scentences = total_positive_sentences / (total_positive_sentences + total_negative_sentences)
         self.percent_negative_scentences = total_negative_sentences / (total_positive_sentences + total_negative_sentences)
 
+        #error checking: make sure every word is entried in either positive word list or negative word list
         for word in vocab:
             if word not in self.postive_word_counts:
                 self.postive_word_counts[word] = 0
             if word not in self.negative_word_counts:
                 self.negative_word_counts[word] = 0
 
+        #store data
         self.total_positive_words = total_positive_words
         self.total_negative_words = total_negative_words
         self.vocabulary_length = len(vocab)
@@ -64,17 +73,23 @@ class BayesClassifier():
         """
         predictions = []
 
+        #loop through each sentence
         for sentence in vectors:
+            #log probability of a sentence being positive
             log_positive_probability = math.log(self.percent_positive_scentences)
+            #log probability of a sentence being negative
             log_negative_probability = math.log(self.percent_negative_scentences)
 
             for word in sentence:
+                #add to the total positive and negative log probability
+                #Laplace smoothing 
                 word_positive_probability = (self.postive_word_counts[word] + 1) / (self.total_positive_words + self.vocabulary_length)
                 word_negative_probability = (self.negative_word_counts[word] + 1) / (self.total_negative_words + self.vocabulary_length)
 
                 log_positive_probability += math.log(word_positive_probability)
                 log_negative_probability += math.log(word_negative_probability)
 
+            #predict wether the sentence is positive or negative
             if log_positive_probability > log_negative_probability:
                 predictions.append(1)
             else:
